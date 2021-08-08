8428e00b30a072d52ec9fef8c2fda00b7cb3cc79
daeru-ne <ando.daryl@hotmail.com>
2021-08-08 21:01:01 +0100

    added js logic; updated html, css; mvp

--- main.js
+++ main.js
@@ -1,8 +1,95 @@
 // /* // comment box
 // */ //
 
-// /* // diagnostic 1
+// /* // operational see diagnostic 1
+const meta = {
+  root: document.documentElement,
+  props: {
+    sideLength: "--canvas-length",
+  },
+  elems: {
+    userInput: document.getElementById("userInput"),
+    canvas: document.getElementById("canvas-container"),
+    button: document.getElementById("create-canvas"),
+  },
+  class: {
+    pixel: "pixel",
+    activePixel: "pixel-active",
+    activeCanvas: "canvas-active",
+  },
+};
+const methods = {
+  generateCanvas() {
+    const inputValue = meta.elems.userInput.value;
+    const canvas = meta.elems.canvas;
+    [...canvas.childNodes].forEach((child) => child.remove());
+    meta.root.style.setProperty(meta.props.sideLength, inputValue);
+    canvas.classList.add(meta.class.activeCanvas);
+    for (
+      let totalPixels = 1, totalArea = Math.pow(inputValue, 2);
+      totalPixels <= totalArea;
+      totalPixels++
+    ) {
+      const pixel = document.createElement("div");
+      pixel.classList.add(meta.class.pixel);
+      pixel.addEventListener("mouseover", (detected) => {
+        if (detected) pixel.classList.add(meta.class.activePixel);
+      });
+      canvas.appendChild(pixel);
+    }
+  },
+};
+methods.generateCanvas();
+meta.elems.button.addEventListener("click", (detected) => {
+  if (detected) methods.generateCanvas();
+});
+// */ //
 
+/* // diagnostic 1 -- works!
+const meta = {
+  root: document.documentElement,
+  props: {
+    sideLength: "--canvas-length",
+  },
+  elems: {
+    userInput: document.getElementById("userInput"),
+    canvas: document.getElementById("canvas-container"),
+    button: document.getElementById("create-canvas"),
+  },
+  class: {
+    pixel: "pixel",
+    activePixel: "pixel-active",
+    activeCanvas: "canvas-active",
+  },
+};
+const methods = {
+  generateCanvas() {
+    const inputValue = meta.elems.userInput.value;
+    const canvas = meta.elems.canvas;
+    [...canvas.childNodes].forEach((child) => child.remove());
+    meta.root.style.setProperty(meta.props.sideLength, inputValue);
+    canvas.classList.add(meta.class.activeCanvas);
+    for (
+      let totalPixels = 1, totalArea = Math.pow(inputValue, 2);
+      totalPixels <= totalArea;
+      totalPixels++
+    ) {
+      const pixel = document.createElement("div");
+      pixel.classList.add(meta.class.pixel);
+      pixel.addEventListener("mouseover", (detected) => {
+        if (detected) pixel.classList.add(meta.class.activePixel);
+      });
+      canvas.appendChild(pixel);
+    }
+  },
+};
+// meta.elems.testOutput = document.createElement("p");
+// document.getElementById("main-container").appendChild(meta.elems.testOutput);
+// meta.elems.testOutput.innerText += "Test Output: ";
+methods.generateCanvas();
+meta.elems.button.addEventListener("click", (detected) => {
+  if (detected) methods.generateCanvas();
+});
 // */ //
 
 /* // approach
@@ -34,11 +121,8 @@ div.pixel
   background-color: inherit;
   onHover(): permanently change color by adding .active
 
-  .active
+  .pixel-active
   background-color: different;
-
-  canvasArea
-  grid-template-columns: repeat(canvasArea, 1fr) / repeat(canvasArea, 1fr);
 
   onLoad()
     generateCanvas()
@@ -49,4 +133,13 @@ div.pixel
     for (let totalPixels = 1; totalPixels <= totalArea; totalPixels++) {
       canvasContainer.insert(pixel)
     }
+  
+  process
+    when page loads generateCanvas()
+    when button is clicked generateCanvas()
+    generateCanvas()
+      update .canvas-active side-length: slider value
+      add .canvas-active to #canvas-container
+      create div.pixel
+        add onmouseover event listeners: add .pixel-active
 // */ //
