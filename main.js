c9269780708369a5feab8ff982ff4c62a3998237
daeru-ne <ando.daryl@hotmail.com>
2021-08-08 17:57:21 +0100

    updated application logic approach;

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // /* // comment box
 // */ //
 
+// /* // diagnostic 1
+
+// */ //
+
 /* // approach
 
 single box
@@ -13,4 +17,36 @@ canvas generator -- when button is pressed (re)generate canvas
   container.height / input
 
 --ideas --
+main structure
+  headings
+  config area with slider
+  canvas area
+
+dynamic canvas
+  auto create grid based on slider value
+  insert pixel per grid square with .pixel for styling
+
+div.pixel
+  width: auto;
+  height: auto;
+  margin: 0;
+  padding: 0;
+  background-color: inherit;
+  onHover(): permanently change color by adding .active
+
+  .active
+  background-color: different;
+
+  canvasArea
+  grid-template-columns: repeat(canvasArea, 1fr) / repeat(canvasArea, 1fr);
+
+  onLoad()
+    generateCanvas()
+
+  generateCanvas()
+    const sideLength = canvasArea.value
+    const totalArea = Math.pow(sideLength, 2)
+    for (let totalPixels = 1; totalPixels <= totalArea; totalPixels++) {
+      canvasContainer.insert(pixel)
+    }
 // */ //
