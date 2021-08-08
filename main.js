b013d93a7db4331325bb1f6a5a86043ce2094db6
daeru-ne <ando.daryl@hotmail.com>
2021-08-08 16:59:35 +0100

    added containers: main, config, canvas; added content inside containers with basic css and interactivity;

---
+++ main.js
@@ -0,0 +1,16 @@
+// /* // comment box
+// */ //
+
+/* // approach
+
+single box
+  when hovered, changes color permanently
+  auto resize according to space available inside container
+
+canvas generator -- when button is pressed (re)generate canvas
+  input = how many per x / y axis
+  container.length / input
+  container.height / input
+
+--ideas --
+// */ //
